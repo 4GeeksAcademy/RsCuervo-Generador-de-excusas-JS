fa8ce709a51bb083f633ad634a2557ba567cc336
Richard Cuervo <richardcuer@hotmail.com>
2025-02-08 02:50:13 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,28 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+function excusaRandom(array) {
+  let arrayRandom = Math.floor(Math.random() * array.length)
+  return array[arrayRandom]
+}
+
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying']
+  
+  let whoRandom = excusaRandom(who)
+  console.log(whoRandom)
+  let actionRandom = excusaRandom(action)
+  console.log(actionRandom)
+  let whatRandom = excusaRandom(what)
+  console.log(whatRandom)
+  let whenRandom = excusaRandom(when)
+  console.log(whenRandom)
+  let excusa = whoRandom + " " + actionRandom + " " + whatRandom + " " + whenRandom;
+  document.getElementById("excuse").innerText = excusa;
+
 };
+
